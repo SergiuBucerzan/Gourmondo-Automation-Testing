e4048dd835e99a6ef57cd89ca4995c6b6c778bf5
Mocian Ciprian <ciprian.mocian@evozon.com>
2016-10-18 19:58:32 +0300

    RegistrationTest

--- src/test/java/com/test/tests/RegistrationTest.java
+++ src/test/java/com/test/tests/RegistrationTest.java
@@ -1,7 +1,12 @@
 package com.test.tests;
 
+import org.junit.runner.RunWith;
+
 import com.test.BaseTest;
 
+import net.serenitybdd.junit.runners.SerenityRunner;
+
+@RunWith (SerenityRunner.class)
 public class RegistrationTest extends BaseTest{
 
 }
