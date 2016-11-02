38392bf4894eb0394a056542aa4e6d9ba584610c
Mocian Ciprian <ciprian.mocian@evozon.com>
2016-11-02 16:04:01 +0200

    CartSteps anotations

--- src/test/java/com/steps/frontend/CartSteps.java
+++ src/test/java/com/steps/frontend/CartSteps.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.steps.AbstractSteps;
 import com.tools.models.CartEntryModel;
 
+import net.thucydides.core.annotations.Step;
+
 public class CartSteps extends AbstractSteps {
 
 	/**
@@ -12,18 +14,22 @@ public class CartSteps extends AbstractSteps {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Step
 	public void selectProductsFromCart() {
 		getCartPAge().selectProductsFromCart();
 	}
 
+	@Step
 	public void deleteSelectedItems() {
 		getCartPAge().deleteSelectedItems();
 	}
 
+	@Step
 	public void goToHomePage() {
 		getCartPAge().goToHomePage();
 	}
 
+	@Step
 	public void pay() {
 		getCartPAge().pay();
 	}
@@ -43,18 +49,22 @@ public class CartSteps extends AbstractSteps {
 
 	// VALIDATIONS
 
+	@Step
 	public void validateCartEntryTotal(List<CartEntryModel> productList) {
 		getCartPAge().validateCartEntryTotal(productList);
 	}
 
+	@Step
 	public void validateCartTotal(double cartCalculatedTotal, double cartTotal) {
 		getCartPAge().validateCartTotal(cartCalculatedTotal, cartTotal);
 	}
 
+	@Step
 	public void validateTotalCartAndTotalValueOfAddedProducts(double addedProductsTotal, double cartTotal) {
 		getCartPAge().validateTotalCartAndTotalValueOfAddedProducts(addedProductsTotal, cartTotal);
 	}
 
+	@Step
 	public void validateNoOfAddedProductsWithNoOfCartProducts(int addedProducts, int cartProducts) {
 		getCartPAge().validateNoOfAddedProductsWithNoOfCartProducts(addedProducts, cartProducts);
 	}
