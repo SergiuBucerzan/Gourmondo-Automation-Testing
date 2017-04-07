e306cf182168315eab8022b2a50d32b0455e68d6
Mocian Ciprian <ciprian.mocian@evozon.com>
2017-04-07 17:17:19 +0300

    Email users changed

--- src/test/java/com/pages/frontend/TestAnything.java
+++ src/test/java/com/pages/frontend/TestAnything.java
@@ -6,7 +6,7 @@ import com.tools.gmail.JavaMailAPIConnector;
 public class TestAnything {
 
 	public static void main(String[] args) {
-		JavaMailAPIConnector emailConnector = new JavaMailAPIConnector(JavaMailAPIConstants.IMAP_GMAIL, JavaMailAPIConstants.IMAPS, JavaMailAPIConstants.MAIL_STORE_PROTOCOL, JavaMailAPIConstants.EMAIL, JavaMailAPIConstants.PASSWORD);
+		JavaMailAPIConnector emailConnector = new JavaMailAPIConnector(JavaMailAPIConstants.IMAP_GMAIL, JavaMailAPIConstants.IMAPS, JavaMailAPIConstants.MAIL_STORE_PROTOCOL, "", "");
 		String text = emailConnector.searchEmail("info@gourmondo.de", "Mitteilung von www.gourmondo.de");
 		String link = emailConnector.getLinkFromEmail(text, "border-radius: 5px;\" href=\"", "\">PASSWORT");
 		System.out.println(link);
--- src/test/java/com/tools/constants/JavaMailAPIConstants.java
+++ src/test/java/com/tools/constants/JavaMailAPIConstants.java
@@ -6,7 +6,5 @@ public class JavaMailAPIConstants {
 		public static final String IMAP_YAHOO = "imap.mail.yahoo.com";
 		public static final String MAIL_STORE_PROTOCOL = "mail.store.protocol";
 		public static final String IMAPS = "imaps";
-		public static final String EMAIL = "ciprian.mocian@gmail.com";		
-		public static final String PASSWORD = "Parola33.";
 	
 }
