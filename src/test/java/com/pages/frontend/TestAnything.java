5e1fe91ab57f9fff22a93e0a1feecc2fc0efce15
Mocian Ciprian <ciprian.mocian@evozon.com>
2017-05-10 09:50:07 +0300

    Java Mail

--- src/test/java/com/pages/frontend/TestAnything.java
+++ src/test/java/com/pages/frontend/TestAnything.java
@@ -9,9 +9,9 @@ public class TestAnything {
 
 	public static void main(String[] args) {
 		JavaMailAPIConnector emailConnector = new JavaMailAPIConnector(JavaMailAPIConstants.MAIL_STORE_PROTOCOL, JavaMailAPIConstants.IMAPS, JavaMailAPIConstants.IMAP_GMAIL, "ciprian.mocian@gmail.com", "No.Smoking.3");
-		//String text = emailConnector.searchEmail("info@gourmondo.de", "Mitteilung von www.gourmondo.de");
+		String text = emailConnector.searchEmail("info@gourmondo.de", "Mitteilung von www.gourmondo.de");
 		//String link = emailConnector.getLinkFromEmail(text, "border-radius: 5px;\" href=\"", "\">PASSWORT");
-		String text = emailConnector.searchEmail("ciprian.mocian@evozon.com", "testest");
+		//String text = emailConnector.searchEmail("ciprian.mocian@evozon.com", "testest");
 		
 		System.out.println(text);
 
